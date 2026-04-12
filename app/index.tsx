ccbb7c1700f1489da4759dc75edf94278793b00a
Carlos <println_g1@163.com>
2026-04-13 00:06:52 +0800

    feat:add native wind v5

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
+import '@/global.css';
 import { Text, View } from 'react-native';
 
-const Index = () => (
-    <View
-        style={{
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-        }}
-    >
-        <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-);
+const App = () => {
+    return (
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-2xl font-bold text-blue-500">Welcome to Nativewind!</Text>
+        </View>
+    );
+};
 
-export default Index;
+export default App;
